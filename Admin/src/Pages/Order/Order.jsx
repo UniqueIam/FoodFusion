f1f9fd2ef4ca9cdfff65e64b131c52743c619b10
Abhimanyu Kumar <abhimanyukumar83097@gmail.com>
2024-07-27 17:59:50 +0530

    api for the admin orders page

--- Admin/src/Pages/Order/Order.jsx
+++ Admin/src/Pages/Order/Order.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
 
-const Order = () => {
+const Order = ({url}) => {
+
+  const [orders,setOrders ] = useState([]);
+
+  const fetchAllOrders = async() =>{
+    const response = await axios.get(url+"/api/order/list")
+    if(response.data.success){
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    }
+    else{
+      toast.error("Error");
+    }
+  }
+
+  useEffect(()=>{
+    fetchAllOrders();
+  },[])
+
   return (
-    <div>
-      orderdsadsadsa
+    <div >
+      
     </div>
   )
 }
--- Backened/src/routes/order.route.js
+++ Backened/src/routes/order.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { placeOrder, userOrders, verifyOrder } from "../controllers/order.controller.js";
+import { listOrders, placeOrder, userOrders, verifyOrder } from "../controllers/order.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const orderRouter = express.Router();
@@ -7,5 +7,6 @@ const orderRouter = express.Router();
 orderRouter.post("/placeorder",authMiddleware,placeOrder);
 orderRouter.post("/verify",verifyOrder);
 orderRouter.post("/userorders",authMiddleware,userOrders);
+orderRouter.get("/list",listOrders);
 
 export default orderRouter;
